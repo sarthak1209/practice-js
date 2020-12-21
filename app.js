40a197ce2bd10ec0c7b44ba13c2d9254f18e4a60
Sarthak Lav <sarthakchaudhary52@gmail.com>
2020-12-21 10:46:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var inputText = document.querySelector("#txt-input");
 //console.log(inputText.value);       
-var submitbtn = document.querySelector('#btn-submit');
-var outputDiv = document.querySelector('#div-output');
+var submitbtn = document.querySelector("#btn-submit");
+var outputDiv = document.querySelector("#div-output");
 
 var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
@@ -9,6 +9,7 @@ function url(input){
     return serverUrl+ "?text="+ input
 }
 function onClickSubmitButton(){
+    console.log("The function is called!!");
     var input = inputText.value;
    fetch(url(input)).then(json => res.json()).then(json => {
 outputDiv.innerText = json.contents;
@@ -17,4 +18,6 @@ outputDiv.innerText = json.contents;
 
 }
 
-submitbtn.addEventListener("click",onClickSubmitButton);
+if(submitbtn){
+    submitbtn.addEventListener("click",onClickSubmitButton);
+}
