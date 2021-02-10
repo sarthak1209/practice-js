951552bc22e0ecf0e152ea957f08bb675d3122cf
Sarthak Lav <sarthakchaudhary52@gmail.com>
2021-02-10 22:18:47 +0530

    basic js practice brush up

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-var inputText = document.querySelector("#txt-input");
-//console.log(inputText.value);       
-var submitbtn = document.querySelector("#btn-submit");
-var outputDiv = document.querySelector("#div-output");
-
-var serverUrl = "https://api.funtranslations.com/translate/minion.json";
-
-function url(input){
-    return serverUrl+ "?text="+ input
-}
-function onClickSubmitButton(){
-    console.log("The function is called!!");
-    var input = inputText.value;
-   fetch(url(input)).then(res => res.json()).then(json => {
-       console.log(json.contents.tranlated);
-outputDiv.innerText = json.contents.tranlated;
-   })
+var inputTxt = document.querySelector('#txt-input');
+var btnClick = document.querySelector('#btn-trans');
+var outputDiv = document.querySelector('#div-output');
 
 
+function clickEventListener() {
+
+    var printText = "Hi " + inputTxt.value + " How are you ??"
+    outputDiv.innerHTML = printText;
 }
 
-
-    submitbtn.addEventListener("click",onClickSubmitButton);
-
+if (btnClick) {
+    btnClick.addEventListener("click", clickEventListener);
+}
